40d224b5688103f7de49bdcfe9fb86ca73c1487b
Kamil Jodłowski <kamil239811@wp.pl>
2024-07-31 20:05:37 +0200

    Dodanie gitignore oraz do admin.py możliwość przeglądania modeli

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Task
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_filter = ("name", "status","assigned_user")
+    list_display = ("name", "status","assigned_user")
+
+
+admin.site.register(Task, TaskAdmin)
